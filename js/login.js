61f07b1e17fc548af81df45252845c458acf65f7
jini <jinhui.ha1208@gmail.com>
2024-10-14 13:27:38 +0900

    로그인 대상 설정 css->  js로적용

--- js/login.js
+++ js/login.js
@@ -1,3 +1,17 @@
+// 로그인 버튼 클릭시 구매회원, 판매회원 전환
+const loginBtns = document.querySelectorAll(".login-btns li button");
+loginBtns.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button === loginBtns[0]) {
+      loginBtns[0].classList.add("active");
+      loginBtns[1].classList.remove("active");
+    } else {
+      loginBtns[1].classList.add("active");
+      loginBtns[0].classList.remove("active");
+    }
+  });
+});
+
 // 로그인 버튼 클릭시 유효성 검사
 // 아이디, 비밀번호 공란 또는 비밀번호만 입력: 아이디를 입력해주세요.
 // 아이디만 입력 : 비밀번호를 입력해주세요.
@@ -5,14 +19,11 @@
 const loginForm = document.querySelector(".login-form");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const iptId = loginForm.id;
-  const iptPw = loginForm.password;
+
+  const idValue = loginForm.id.value;
+  const pwValue = loginForm.password.value;
   const msg = loginForm.querySelector("p");
   msg.classList.add("login-error");
-
-  const idValue = iptId.value;
-  const pwValue = iptPw.value;
-  console.log(iptId);
 
   // input 태그 내 value 값이 있는지 검사
   if ((!idValue && !pwValue) || (!idValue && pwValue)) {
@@ -27,6 +38,4 @@ loginForm.addEventListener("submit", (e) => {
 });
 
 /* 구현할 기능 */
-// 로그인 버튼 클릭시 구매회원, 판매회원 전환
-
 //API 적용
