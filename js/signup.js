e756b8794ba04f5f959439f42993ac5ccaa63560
jini <jinhui.ha1208@gmail.com>
2024-10-15 09:39:37 +0900

    id 유효성 검증 css 적용

--- js/signup.js
+++ js/signup.js
@@ -19,7 +19,20 @@ joinBtns.forEach((button) => {
 // 이미 사용중인 아이디인경우 : 이미 사용 중인 아이디 입니다.
 // 사용 가능한 아이디인 경우 : 멋진 아이디네요 :)
 const joinForm = document.querySelector(".join-form");
-joinForm.addEventListener("submit", (e) => {});
+const idMsg = joinForm.querySelector(".id-check");
+const idBtn = joinForm.querySelectorAll(".duplicate-check");
+idBtn[0].addEventListener("click", (e) => {
+  e.preventDefault();
+  // if 중복확인 체크 후 사용중인 아이디인경우
+  idMsg.textContent = "이미 사용 중인 아이디 입니다.";
+  idMsg.classList.add("id-error");
+  idMsg.classList.remove("id-check");
+
+  // if 사용 가능한 아이디인 경우
+  idMsg.textContent = "멋진 아이디네요 :)";
+  idMsg.classList.remove("id-error");
+  idMsg.classList.add("id-check");
+});
 
 // 비밀번호 유효성 검증
 // 비밀번호가 유효하게 입력되었으면 체크 표시 -> 초록색 체크 표시
@@ -37,10 +50,7 @@ joinForm.addEventListener("input", (e) => {
   }
 
   // 비밀번호 일치 확인
-  if (
-    !password.validity.valueMissing &&
-    !passwordRecheck.validity.valueMissing
-  ) {
+  if (password.validity.valid && !passwordRecheck.validity.valueMissing) {
     if (password.value !== passwordRecheck.value) {
       pwMsg.textContent = "비밀번호가 일치하지 않습니다.";
       pwMsg.classList.add("join-error");
@@ -54,3 +64,25 @@ joinForm.addEventListener("input", (e) => {
     }
   }
 });
+
+// API 기능 구현
+
+// fetch('https://estapi.openmarket.weniv.co.kr/accounts/buyer/signup/', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     "username": String,
+// 		"password": String,
+// 		"name": String,
+// 		"phone_number": String,
+//   })
+// })
+
+// fetch("https://estapi.openmarket.weniv.co.kr/")
+//   .then((response) => response)
+//   .then((json) => console.log(json))
+//   .catch((error) => console.error(error));
+
+console.log(joinForm.querySelector("#id"));
