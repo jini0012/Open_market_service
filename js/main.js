93671a68929026d94c05b470c5782a1d59fa9be9
jini <jinhui.ha1208@gmail.com>
2024-10-17 09:07:01 +0900

    📝 주석 수정

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 로그인 버튼 -> access랑 refresh를 location에 저장을 하는데
+// 로그아웃했을 때 이부분이 값이 비도록 만들고,
+// 로그인을 하고 난 main 창에서 access랑 refresh가 location에 저장이 되어있으면 -> login을 hidden
+// 저장이 안되어있으면 마이 페이지를 hidden
+
+// 판매자 페이지는 토큰이 다를거같으니까? 확인하고 토큰값이 ~~면 판매자 페이지가 보이게끔 (판매자 보이고 장바구니 hidden, 로그인 hidden)
+
+// 로그아웃버튼을 누르면 access값, refresh값을 삭제하고 마이페이지 hidden, 로그인 visible
