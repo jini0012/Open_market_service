8aad734b212e4c6fa5813dd95adef128430e1e3a
jini <jinhui.ha1208@gmail.com>
2024-10-17 18:23:26 +0900

    🎨 상품 상세 페이지 - productId 이용하여 적용

--- js/goods.js
+++ js/goods.js
@@ -1,3 +1,23 @@
+const goodsImg = document.querySelector(".buy-wrap img");
+const goodsInfo = document.querySelector(".goodsInfo");
+
+// 페이지를 열었을 때 이미지, 상품명, 가격 등이 변경되어 보이도록 설정한다.
+fetch(
+  `https://estapi.openmarket.weniv.co.kr/products/${localStorage.productId}`
+)
+  .then((response) => response.json())
+  .then((json) => {
+    // 페이지가 열렸을 때
+    // 이미지 -> fetch내 링크의 이미지, 상품명, 가격 적용
+    console.log(json);
+    goodsImg.src = `${json.image}`;
+    goodsInfo.querySelector("p").textContent = `${json.name}`;
+    goodsInfo.querySelector("h3").textContent = `${json.info}`;
+    goodsInfo.querySelector("span").textContent = `${json.price}`;
+    localStorage.setItem("price", json.price);
+  })
+  .catch((error) => console.error(error));
+
 // 상품 클릭 시 document.title HODU : 상품 이름으로 변경되게 적용
 // 모달창 임시 스크립트
 // 로그인이 되지 않은 상태일때만 버튼을 눌렀을 때 모달창이 떠야한다.
@@ -45,10 +65,18 @@ const Btns = form.querySelectorAll("button");
 const num = form.num;
 let total = form.querySelector(".count");
 let totalPrice = form.querySelector(".totalPrice");
-const defaultPrice = totalPrice.textContent
+// const defaultPrice = totalPrice.textContent
+//   .split("")
+//   .filter((elem) => elem >= 0)
+//   .join("");
+
+// 가격 로컬스토리지에 저장(문자열타입) 후 불러와서 사용
+const defaultPrice = localStorage.price
   .split("")
   .filter((elem) => elem >= 0)
   .join("");
+// 초기 값 설정
+totalPrice.textContent = defaultPrice;
 
 // 재고 없는 경우 plus 버튼 disabled
 if (plus.disabled === true) {
