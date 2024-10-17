45b9d337125870534916da629fd90ad0d146a0cf
jini <jinhui.ha1208@gmail.com>
2024-10-17 13:52:37 +0900

    🎨 로그인 동작 js 추가 ✅ 구현 기능 체크

--- js/login.js
+++ js/login.js
@@ -1,3 +1,20 @@
+// 로그인창 실행시 로컬스토리지 초기화
+localStorage.removeItem("accessToken");
+localStorage.removeItem("refreshToken");
+localStorage.removeItem("username");
+localStorage.removeItem("name");
+localStorage.removeItem("phone");
+localStorage.removeItem("type");
+
+// 로그인 타입을 지정하는 함수
+function loginType() {
+  if (loginBtns[0].classList.contains("active")) {
+    return "BUYER";
+  } else if (loginBtns[1].classList.contains("active")) {
+    return "SELLER";
+  }
+}
+
 // 로그인 구매회원, 판매회원 버튼 전환
 const loginBtns = document.querySelectorAll(".login-btns li button");
 loginBtns.forEach((button) => {
@@ -9,6 +26,8 @@ loginBtns.forEach((button) => {
       loginBtns[1].classList.add("active");
       loginBtns[0].classList.remove("active");
     }
+
+    console.log(loginType());
   });
 });
 
@@ -17,6 +36,7 @@ loginBtns.forEach((button) => {
 // 아이디만 입력 : 비밀번호를 입력해주세요.
 // 아이디, 비밀번호가 일치하지 않을 경우 : 아이디 또는 비밀번호가 일치하지 않습니다.
 const loginForm = document.querySelector(".login-form");
+
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -26,10 +46,13 @@ loginForm.addEventListener("submit", (e) => {
   msg.classList.add("login-error");
 
   // input 태그 내 value 값이 있는지 검사
+  // input 태그 미입력 시 focus 이벤트
   if ((!idValue && !pwValue) || (!idValue && pwValue)) {
     msg.innerText = `아이디를 입력해주세요`;
+    loginForm.id.focus();
   } else if (!pwValue) {
     msg.innerText = `비밀번호를 입력해주세요`;
+    loginForm.password.focus();
   } else {
     msg.innerText = ``;
   }
@@ -43,7 +66,7 @@ loginForm.addEventListener("submit", (e) => {
     body: JSON.stringify({
       username: idValue,
       password: pwValue,
-      login_type: "BUYER",
+      login_type: `${loginType()}`,
       // BUYER : 일반 구매자, SELLER : 판매자
     }),
   })
@@ -57,11 +80,15 @@ loginForm.addEventListener("submit", (e) => {
         localStorage.setItem("name", `${json.user.name}`);
         localStorage.setItem("phone", `${json.user.phone_number}`);
         localStorage.setItem("type", `${json.user.user_type}`);
-        // 로그인 완료 후 페이지 이동
-        location.href = "./index.html";
+        // 로그인 완료 후 페이지 이전 페이지로 이동
+        location.href = `${document.referrer}`;
       } else if (pwValue) {
         // 아이디만 입력했을 때 '비밀번호를 입력해주세요'가 나오지 않는 오류 방지
         msg.innerText = `아이디 또는 비밀번호가 일치하지 않습니다.`;
+        // 아이디, 비밀번호 미일치 되었을 때 빈칸으로 적용 및 비밀번호칸 focus 적용
+        loginForm.id.value = "";
+        loginForm.password.value = "";
+        loginForm.password.focus();
       }
     })
     .catch((error) => console.error(error));
