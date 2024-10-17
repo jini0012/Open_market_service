964030bafc173d45fe0db6be2f5c973d19ffd0a5
jini <jinhui.ha1208@gmail.com>
2024-10-17 17:50:04 +0900

    🎨 상품 목록 불러오기 및 목록페이지 업데이트  js 구현

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-// 판매자 페이지는 토큰이 다를거같으니까? 확인하고 토큰값이 ~~면 판매자 페이지가 보이게끔 (판매자 보이고 장바구니 hidden, 로그인 hidden)
+const products = document.querySelectorAll("article");
+// 상품 전체 불러오기
+fetch("https://estapi.openmarket.weniv.co.kr/products/")
+  .then((response) => response.json())
+  .then((json) => {
+    // 불러온 후 이미지, 값 등을 페이지에 넣는 방식으로 저장
+    products.forEach((product, index) => {
+      product.querySelector("img").src = `${json.results[index].image}`;
+      product.querySelector(
+        "a"
+      ).href = `https://estapi.openmarket.weniv.co.kr/products/${[index + 1]}`;
+      product.querySelector("p").textContent = `${json.results[index].name}`;
+      product.querySelector("h3").textContent = `${json.results[index].info}`;
+      product.querySelector(
+        "span"
+      ).textContent = `${json.results[index].price}`;
+
+      // 상품을 눌렀을 때 상세페이지로 이동하기 => 상품 상세 페이지 + 변경된 이미지 + 내용 추가 필요
+      product.addEventListener("click", (e) => {});
+    });
+  })
+  .catch((error) => console.error(error));
