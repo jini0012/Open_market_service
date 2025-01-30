080fc97450a5dff1cfd39db7d52ad3951b36391b
jini0012 <jinhui.ha1208@gmail.com>
2025-01-30 18:47:15 +0900

    🎨 token 재발급 기능 함수로 분리

--- js/common.js
+++ js/common.js
@@ -24,31 +24,35 @@ function logout() {
   location.reload(true);
 }
 
+function getAccessToken() {
+  fetch(`${fetchUrl}/accounts/token/refresh/`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      refresh: localStorage.refreshToken,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        alert(
+          "로그인 세션이 만료되어 로그아웃되었습니다. 재로그인을 진행해주세요."
+        );
+        logout();
+      }
+      return response.json();
+    })
+    .then((json) => {
+      localStorage.setItem("accessToken", `${json.access}`);
+    })
+    .catch((error) => console.error(error));
+}
+
 if (localStorage.accessToken) {
   if (localStorage.accessToken !== "undefined") {
     setInterval(() => {
-      fetch(`${fetchUrl}/accounts/token/refresh/`, {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          refresh: localStorage.refreshToken,
-        }),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            alert(
-              "로그인 세션이 만료되어 로그아웃되었습니다. 재로그인을 진행해주세요."
-            );
-            logout();
-          }
-          return response.json();
-        })
-        .then((json) => {
-          localStorage.setItem("accessToken", `${json.access}`);
-        })
-        .catch((error) => console.error(error));
+      getAccessToken();
     }, 300000);
   }
 
