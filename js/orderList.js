7870da5d09d34f243cce1505bdb3eaab9e1ed035
jini0012 <jinhui.ha1208@gmail.com>
2025-02-05 02:32:01 +0900

    🎨 구매자명 h2 내 적용

--- js/orderList.js
+++ js/orderList.js
@@ -5,3 +5,7 @@ if (
   alert("비정상적인 접속 경로 입니다. 메인페이지로 이동합니다.");
   location.href = "index.html";
 }
+const user = JSON.parse(decodeURIComponent(localStorage.user)).name;
+
+const main = document.querySelector("main");
+main.querySelector("h2 span").textContent = user;
