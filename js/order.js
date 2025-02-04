cedf9e334a55367050120a3aa149ff95e9477e87
jini0012 <jinhui.ha1208@gmail.com>
2025-02-05 00:02:33 +0900

    🎨 바로 주문하기 링크 이동 예외처리

--- js/order.js
+++ js/order.js
@@ -1,5 +1,9 @@
 localStorage.removeItem("product_info");
-if (!localStorage.getItem("type") || localStorage.getItem("type") !== "BUYER") {
+if (
+  !localStorage.getItem("type") ||
+  localStorage.getItem("type") !== "BUYER" ||
+  !localStorage.getItem("orderItem")
+) {
   alert("비정상적인 접속 경로 입니다. 메인페이지로 이동합니다.");
   location.href = "index.html";
 }
