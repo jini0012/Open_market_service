6e156d44b925b32c1781e63735bf2b1679e8dd54
jini0012 <jinhui.ha1208@gmail.com>
2025-02-06 15:42:17 +0900

    ✨ 접속 경로 예외처리 적용

--- js/sellerGoods.js
+++ js/sellerGoods.js
@@ -1,3 +1,8 @@
+if (localStorage.getItem("type") !== "SELLER") {
+  alert("비정상적인 접속 경로 입니다. 메인페이지로 이동합니다.");
+  location.href = "index.html";
+}
+
 const uploadForm = document.querySelector("form");
 const radioButtons = uploadForm.querySelectorAll('input[name="delivery"]');
 let radioSelected = "PARCEL";
